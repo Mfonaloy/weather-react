4fc6930c42d4046a4985aacb23bf64b0fff0524d
Mfon Aloy <mfonobongsmn82@gmail.com>
2023-08-04 23:21:02 +0100

    Edited the link.

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function footer() {
     return <div className="footer">
       <small>
-         <a href="https://github.com/Mfonaloy/vanilla-weather-app" target="_blank" rel="noopener noreferrer">
+         <a href="https://github.com/Mfonaloy/weather-react" target="_blank" rel="noopener noreferrer">
          Open-source code</a> by Mfon Aloy
         </small>
     </div>;
